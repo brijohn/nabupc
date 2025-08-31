b0a07241b593deb3201d272a01b08893ac4b1497
Brian Johnson <brijohn@gmail.com>
2025-08-31 14:50:51 -0400

    nabu_coleco: Update emulation vector to address 0x2100

--- games/nabu_coleco/vlink_nabu.cmd
+++ games/nabu_coleco/vlink_nabu.cmd
@@ -4,9 +4,9 @@ SECTIONS {
 	.crt0 0x140d : {
 		*(.crt0 .crt0.*)
 	}
-	PROVIDE(reloc_start = .);
-	.text 0x2100 : AT(.)  {*(.text .text.*)}
+	.text 0x2110 : AT(.)  {*(.text .text.*)}
 	.data : {*(.data .data.*)}
 	PROVIDE(reloc_end = LOADADDR(.data) + SIZEOF(.data));
 	PROVIDE(reloc_length = SIZEOF(.text) + SIZEOF(.data));
+	PROVIDE(reloc_start = 0x2110 + reloc_length - 1);
 }
